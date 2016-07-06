9cd3aab8adb88a47325227815ca854c38f15fc5c
deepanshu97 <dcthegamer@gmail.com>
2016-07-06 13:32:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 $to='deepanshuchauhan.02@gmail.com';
        $subject='Contact Mail';
        $body = 'Hello';
-       $headers = "From: someone@gmail.com"
-       if(@mail($to,$subject,$body,$headers))
+       $headers = "From: someone@gmail.com";
+       if(mail($to,$subject,$body,$headers))
        {
        	echo 'Email has been successfully sent';
        } 
        else
        {
-       	echo 'Some error occured. Please try again later';
+       	echo 'Some error occured . Please try again later';
        }
 ?>
